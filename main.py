8f04e3d5fafb72a1eda43bbd58816c0c388106aa
Ferran Pares <ferran.pares.pont@gmail.com>
2014-10-20 14:48:06 +0200

    Ho he separat en funcions tota la creació de Diccionaris per a que sigui més senzill, les funcions per cada tipus de smooth, segueixen per separat

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def computeModel(pathfile):
 	Bentropy = 0.0
 	Tentropy = 0.0
 	length = float(len(Lwords))
-	#length = float(100554)
 	prob = {}
 	Bprob = {}
 
@@ -44,17 +43,13 @@ def computeModel(pathfile):
 def computeTaggedModel (pathfile, part):
 	LofPwords = getTaggedWordsFromFile(pathfile)
 	Lwords = map(lambda x:x[0],LofPwords)
-	#Ltags = map(lambda x:x[1],LofPwords)
 	Nwords = len(Lwords)
-	#Ntags = len(Ltags)
 	Tperplexity = 0.0
 
 	for i in range(part):
 		Tentropy = 0.0
 		(U,B,T) = countNgrams(Lwords,i*Nwords/part,(i+1)*Nwords/part)
-		#(Ut, Bt, Tt) = countNgrams(Ltags,i*Ntags/part,(i+1)*Ntags/part)
 		length = float(Nwords/part)
-		#lengthtags = float(Ntags/part)
 		prob = {}
 		Bprob = {}
 
@@ -82,20 +77,46 @@ def computeTaggedModel (pathfile, part):
 
 	return (Tperplexity)
 
-def getTagfromWord(LPairs):
-	wordtagDIC = {}
+def getDicsfromPairs(LPairs, B, T):
+	Btw = {}
+	Btt = {}
+	Ttww = {}
+	Tttw = {}
+	wordTagDIC = {}
 	for i in range(len(LPairs)):
-		if LPairs[i][0] not in wordtagDIC:
-			wordtagDIC[LPairs[i][0]] = LPairs[i][1]
+		if LPairs[i][0] not in wordTagDIC:
+			wordTagDIC[LPairs[i][0]] = LPairs[i][1]
 
-	return wordtagDIC
+	for bkey in B.keys():
+		if (wordTagDIC[bkey[0]],bkey[1]) not in Btw:
+			Btw[wordTagDIC[bkey[0]],bkey[1]] = B[bkey]
+		else:
+			Btw[wordTagDIC[bkey[0]],bkey[1]] += B[bkey]
+			
+		if (wordTagDIC[bkey[0]],wordTagDIC[bkey[1]]) not in Btt:
+			Btt[wordTagDIC[bkey[0]],wordTagDIC[bkey[1]]] = B[bkey]
+		else:
+			Btt[wordTagDIC[bkey[0]],wordTagDIC[bkey[1]]] += B[bkey]
+
+	for tkey in T.keys():
+		if (wordTagDIC[tkey[0]],tkey[1],tkey[2]) not in Ttww:
+			Ttww[wordTagDIC[tkey[0]],tkey[1],tkey[2]] = T[tkey]
+		else:
+			Ttww[wordTagDIC[tkey[0]],tkey[1],tkey[2]] += T[tkey]
+			
+		if (wordTagDIC[tkey[0]],wordTagDIC[tkey[1]],tkey[2]) not in Tttw:
+			Tttw[wordTagDIC[tkey[0]],wordTagDIC[tkey[1]],tkey[2]] = T[tkey]
+		else:
+			Tttw[wordTagDIC[tkey[0]],wordTagDIC[tkey[1]],tkey[2]] += T[tkey]
+
+	return (wordTagDIC,Btw,Btt,Ttww,Tttw)
 
 
 def computeTaggedModelTWW (pathfile, part):
 	LPairs = getTaggedWordsFromFile(pathfile)
 	Lwords = map(lambda x:x[0],LPairs)
 	Ltags = map(lambda x:x[1],LPairs)
-	wordTagDIC = getTagfromWord(LPairs)
+	#wordTagDIC = getTagfromWord(LPairs)
 	Nwords = len(Lwords)
 	Ntags = len(Ltags)
 	Tperplexity = 0.0
@@ -105,23 +126,15 @@ def computeTaggedModelTWW (pathfile, part):
 		(U,B,T) = countNgrams(Lwords,i*Nwords/part,(i+1)*Nwords/part)
 		(Ut, Bt, Tt) = countNgrams(Ltags,i*Ntags/part,(i+1)*Ntags/part)
 		length = float(Nwords/part) #equal length of tags
-		#lengthtags = float(Ntags/part)
 		prob = {}
 		Bprob = {}
+		wordTagDIC = {}
 		Btw = {}
+		Btt = {}
 		Ttww = {}
+		Tttw = {}
 		
-		for bkey in B.keys():
-			if (wordTagDIC[bkey[0]],bkey[1]) not in Btw:
-				Btw[wordTagDIC[bkey[0]],bkey[1]] = B[bkey]
-			else:
-				Btw[wordTagDIC[bkey[0]],bkey[1]] += B[bkey]
-
-		for tkey in T.keys():
-			if (wordTagDIC[tkey[0]],tkey[1],tkey[2]) not in Ttww:
-				Ttww[wordTagDIC[tkey[0]],tkey[1],tkey[2]] = T[tkey]
-			else:
-				Ttww[wordTagDIC[tkey[0]],tkey[1],tkey[2]] += T[tkey]
+		(wordTagDIC, Btw, Btt, Ttww, Tttw) = getDicsfromPairs(LPairs, B, T)
 
 		for key in Ut.keys():
 			pX = Ut[key]/length
@@ -151,7 +164,7 @@ def computeTaggedModelTTW (pathfile, part):
 	LPairs = getTaggedWordsFromFile(pathfile)
 	Lwords = map(lambda x:x[0],LPairs)
 	Ltags = map(lambda x:x[1],LPairs)
-	wordTagDIC = getTagfromWord(LPairs)
+	#wordTagDIC = getTagfromWord(LPairs)
 	Nwords = len(Lwords)
 	Ntags = len(Ltags)
 	Tperplexity = 0.0
@@ -164,20 +177,15 @@ def computeTaggedModelTTW (pathfile, part):
 		#lengthtags = float(Ntags/part)
 		prob = {}
 		Bprob = {}
+		wordTagDIC = {}
+		Btw = {}
 		Btt = {}
+		Ttww = {}
 		Tttw = {}
 		
-		for bkey in B.keys():
-			if (wordTagDIC[bkey[0]],wordTagDIC[bkey[1]]) not in Btt:
-				Btt[wordTagDIC[bkey[0]],wordTagDIC[bkey[1]]] = B[bkey]
-			else:
-				Btt[wordTagDIC[bkey[0]],wordTagDIC[bkey[1]]] += B[bkey]
-
-		for tkey in T.keys():
-			if (wordTagDIC[tkey[0]],wordTagDIC[tkey[1]],tkey[2]) not in Tttw:
-				Tttw[wordTagDIC[tkey[0]],wordTagDIC[tkey[1]],tkey[2]] = T[tkey]
-			else:
-				Tttw[wordTagDIC[tkey[0]],wordTagDIC[tkey[1]],tkey[2]] += T[tkey]
+		(wordTagDIC, Btw, Btt, Ttww, Tttw) = getDicsfromPairs(LPairs, B, T)
+		
+		
 
 		for key in Ut.keys():
 			pX = Ut[key]/length
@@ -206,7 +214,7 @@ def computeTaggedModelTTW (pathfile, part):
 	
 
 
-
+'''
 t0 = time()
 (UEntropy, BEntropy, TEntropy) = computeModel("corpus/en.txt")
 t01 = time()
@@ -218,8 +226,8 @@ t11 = time()
 print (t11-t1),'s -- Entropies of spanish corpus: ',UEntropyes, BEntropyes, TEntropyes
 
 print 'Relation between entropies in english and spanish', UEntropy/UEntropyes, BEntropy/BEntropyes, TEntropy/TEntropyes
-
-
+'''
+'''
 FPerplexity = computeTaggedModel ("corpus/taggedBrown.txt", 1)
 print 'Perplexity of full Browncorpus: ' , FPerplexity
 
@@ -228,7 +236,7 @@ print 'Perplexity of half Browncorpus: ' , HPerplexity
 
 QPerplexity = computeTaggedModel ("corpus/taggedBrown.txt", 4)
 print 'Perplexity of quarter Browncorpus: ' , QPerplexity
-
+'''
 
 FPerplexityTWW = computeTaggedModelTWW ("corpus/taggedBrown.txt", 1)
 print 'Perplexity of full Browncorpus TWW: ' , FPerplexityTWW
